fce6ba52969e2f12fcf238bdad06a482a451d485
suthirakprom <suthirakprom81@gmail.com>
2022-05-07 23:19:11 +0700

    the code is working now.

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -4,7 +4,13 @@ from scripts.helpful_scripts import get_account
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account}, publish_source=True)
+
+    # pass pricefeed address to fundme contract
+    fund_me = FundMe.deploy(
+        "0x9246f813c1C36D9B816BAb34e7366fD3992d1E5f",
+        {"from": account}, 
+        publish_source=True
+    )
     print(f"Contract deployed to {fund_me.address}")
 
 def main():
