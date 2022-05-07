bc310d5d6c2ba4f77609612b6c2b19184728c32e
suthirakprom <suthirakprom81@gmail.com>
2022-05-07 23:34:00 +0700

    add MT mock contract in test folder

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,4 +1,4 @@
-from brownie import FundMe
+from brownie import FundMe, network, accounts, config
 from scripts.helpful_scripts import get_account
 
 
@@ -6,8 +6,14 @@ def deploy_fund_me():
     account = get_account()
 
     # pass pricefeed address to fundme contract
+    if network.show_active() != "development": 
+        price_feed_address =  config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else: 
+        price_feed_address = accounts[0]
     fund_me = FundMe.deploy(
-        "0x9246f813c1C36D9B816BAb34e7366fD3992d1E5f",
+        price_feed_address,
         {"from": account}, 
         publish_source=True
     )
