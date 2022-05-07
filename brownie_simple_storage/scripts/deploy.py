1ba23224cd25783f5565db2e0f5ddfe400de3a8e
suthirakprom <suthirakprom81@gmail.com>
2022-05-07 13:55:08 +0700

    simplify our deploy.py with brownie

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,8 +1,18 @@
-from brownie import accounts
+from brownie import accounts, config, SimpleStorage
 
 def deploy_simple_storage():
-    account = accounts.load("freecodecamp-account")
-    print(account)
+    # account = accounts.load("freecodecamp-account")
+    
+    # account = accounts.add(config["wallets"]["from_key"])
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(simple_storage)
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
 
 def main():
     deploy_simple_storage()
