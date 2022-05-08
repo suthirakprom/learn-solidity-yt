7baa209faccf87b8c6db9c47e1f6f014a6c02d61
suthirakprom <suthirakprom81@gmail.com>
2022-05-08 14:50:29 +0700

    Create a variable

--- brownie_fund_me/scripts/helpful_scripts.py
+++ brownie_fund_me/scripts/helpful_scripts.py
@@ -1,6 +1,9 @@
 from brownie import network, config, accounts, MockV3Aggregator
 from web3 import Web3
 
+DECIMALS = 18
+STARTING_PRICE = 2000
+
 def get_account():
     if network.show_active() == "development":
         return accounts[0]
@@ -12,8 +15,8 @@ def deploy_mocks():
     print("Deploying Mocks...")
     if len(MockV3Aggregator) <= 0:
         MockV3Aggregator.deploy(
-            18, 
-            Web3.toWei(2000, "ether"), 
+            DECIMALS, 
+            Web3.toWei(STARTING_PRICE, "ether"), 
             {"from": get_account()}
             )
     print("Mocks Deployed!")
