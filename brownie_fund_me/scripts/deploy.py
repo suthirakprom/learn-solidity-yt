8bf1ceeae8caffe57ae7648a86183ab9eb532547
suthirakprom <suthirakprom81@gmail.com>
2022-05-08 14:39:29 +0700

    add some more code to deploy after mockV3

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,4 +1,4 @@
-from brownie import FundMe, network, accounts, config
+from brownie import FundMe, network, accounts, config, MockV3Aggregator
 from scripts.helpful_scripts import get_account
 
 
@@ -11,11 +11,16 @@ def deploy_fund_me():
             "eth_usd_price_feed"
         ]
     else: 
-        price_feed_address = accounts[0]
+        print(f"The active network is {network.show_active()}")
+        print("Deploying Mocks...")
+        mock_aggregator = MockV3Aggregator.deploy(18, 2000000000000000000000, {"from": account})
+        price_feed_address = mock_aggregator.address
+        print("Mocks Deployed!")
+
     fund_me = FundMe.deploy(
         price_feed_address,
         {"from": account}, 
-        publish_source=True
+        publish_source=config["networks"][network.show_active()].get("verify")
     )
     print(f"Contract deployed to {fund_me.address}")
 
